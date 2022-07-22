2fa64a6eeae0c4761ad423335d7caa6e50f98edf
zouber04 <i.zouber04@gmail.com>
2022-07-22 16:20:11 -0400

    factorial: Fixed caching to dictionnary

--- factorial.py
+++ factorial.py
@@ -5,29 +5,34 @@
 #########################
 from collections import defaultdict
 
-def calculate(n):
+def calculate(n,cached_dict):
     
     if n == 1:
           
         return n  
     else:  
         
-        recur = n*calculate(n-1)
+        recur = n*calculate(n-1,cached_dict)
+        cached_dict[n] = recur
         
         return recur
 
 
 def cachedfactorial(num,cached_dict):
-    fact_num = int(num)
-    result = calculate(fact_num)
+    if num in cached_dict:
+        return cached_dict[num]
+
+    result = calculate(num,cached_dict)
     
     cached_dict[num] = result
     
-    return
+    return result
    
 
 
 if __name__ == "__main__":
     numdict = {}
-    num = cachedfactorial('5',numdict)
-    print(numdict)
+    num = cachedfactorial(5,numdict)
+    num2 = cachedfactorial(6,numdict)
+    num3 = cachedfactorial(3,numdict)
+    print(num3)
